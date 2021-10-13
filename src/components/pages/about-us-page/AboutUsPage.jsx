d08f9a4b2a2937567ea91b7e96de5b229d1beb04
Zuka <Zuka@PC-12.owl.local>
2021-10-13 20:38:25 +0400

    feat: implemented about-us page

--- src/components/pages/about-us-page/AboutUsPage.jsx
+++ src/components/pages/about-us-page/AboutUsPage.jsx
@@ -1,9 +1,72 @@
+import SubscribeOffer from 'components/common-components/subscribe-offer/SubscribeOffer';
+import Footer from 'components/components/footer/Footer';
+import Header from 'components/components/header/Header';
+
+import { ReactComponent as LinkedinIcon } from '../../../assets/icons/linkedin-icon.svg';
+import { ReactComponent as InstagramIcon } from '../../../assets/icons/instagram-icon.svg';
+import { ReactComponent as FacebookIcon } from '../../../assets/icons/facebook-icon.svg';
+import { ReactComponent as TiktokIcon } from '../../../assets/icons/tiktok-icon.svg';
+
+import * as S from './about-us-page-components';
+
 const AboutUsPage = () => {
   return (
-    <div style={{ backgroundColor: 'red', height: '100rem' }}>
-      {' '}
-      About Us Page{' '}
-    </div>
+    <>
+      <S.HeaderWrapper>
+        <Header />
+      </S.HeaderWrapper>
+      <S.Wrapper>
+        <S.Body>
+          <S.TextsWrapper>
+            <S.Heading>ჩვენ შესახებ</S.Heading>
+            <S.Text>
+              ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+              მოყვა, ერთ ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე…
+              ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+              მოყვა, ერთ ღონისძიებას მეორე, აქსელერატორს მეორეინოვაციებისა და
+              მეწარმეობის მიმართულებით{' '}
+            </S.Text>
+            <S.SubHeading>
+              ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            </S.SubHeading>
+            <S.Text>
+              ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+              მოყვა, ერთ ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე…
+              ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+              მოყვა, ერთ ღონისძიებას მეორე, აქსელერატორს მეორეინოვაციებისა და
+              მეწარმეობის მიმართულებით{' '}
+            </S.Text>
+          </S.TextsWrapper>
+          <S.ImagesWrapper>
+            <S.Image
+              src="https://www.bbva.com/wp-content/uploads/2018/04/equipo-startup-bbva-1-e1524492124593-1024x521.jpg"
+              alt="about-us"
+            />
+            <S.Image
+              src="https://www.bbva.com/wp-content/uploads/2018/04/equipo-startup-bbva-1-e1524492124593-1024x521.jpg"
+              alt="about-us"
+            />
+            <S.FollowWrapper>
+              <S.FollowText>გამოგვყევი:</S.FollowText>
+              <S.FollowIcons>
+                <InstagramIcon />
+                <FacebookIcon />
+                <LinkedinIcon />
+                <TiktokIcon />
+              </S.FollowIcons>
+            </S.FollowWrapper>
+          </S.ImagesWrapper>
+        </S.Body>
+        <S.SubscribeWrapper>
+          <SubscribeOffer
+            cicrcleColor="#55AA79"
+            arrowColor="#55AA79"
+            inputColor="#FE7760"
+          />
+        </S.SubscribeWrapper>
+        <Footer />
+      </S.Wrapper>
+    </>
   );
 };
 
---
+++ src/components/pages/about-us-page/about-us-page-components.jsx
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: relative;
+  padding-top: 9.8rem;
+  background-color: #f2f1f1;
+`;
+
+export const HeaderWrapper = styled.div`
+  position: fixed;
+  z-index: 10;
+  width: 100%;
+  padding: 0rem 10%;
+  background-color: #ffffff;
+`;
+
+export const Body = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 800px;
+  padding: 2.5rem 0rem 6rem 0rem;
+`;
+
+export const TextsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 3rem;
+  max-width: 54rem;
+`;
+
+export const ImagesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SubscribeWrapper = styled.div`
+  background-color: #ffffff;
+  height: 25.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 3rem;
+`;
+
+export const Image = styled.img`
+  width: 44.4rem;
+  height: 30rem;
+  margin-bottom: 1.6rem;
+  border-radius: 4rem;
+  object-fit: cover;
+`;
+
+export const Heading = styled.h5`
+  color: #13182b;
+  font-size: 3.2rem;
+  font-weight: 600;
+  margin-bottom: 3.5rem;
+  line-height: 3rem;
+`;
+
+export const SubHeading = styled.p`
+  color: #13182b;
+  font-size: 2.4rem;
+  font-weight: 600;
+  margin-bottom: 2.5rem;
+  line-height: 3rem;
+`;
+
+export const Text = styled.p`
+  color: #13182b;
+  font-size: 1.8rem;
+  font-weight: 400;
+  margin-bottom: 2.5rem;
+  line-height: 2.2rem;
+`;
+
+export const FollowWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 5rem;
+`;
+
+export const FollowText = styled.p`
+  color: #929db9;
+  margin-bottom: 2.6rem;
+  font-size: 1.6rem;
+  font-weight: 400;
+`;
+
+export const FollowIcons = styled.div`
+  display: flex;
+  gap: 1.6rem;
+
+  & > * {
+    cursor: pointer;
+  }
+`;
