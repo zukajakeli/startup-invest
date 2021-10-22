8c4dda5ee9ce1e019d9aa306e68b9f403bf426e6
Zuka <Zuka@PC-12.owl.local>
2021-10-23 00:53:13 +0400

    feat: implemented mobile single blog

--- src/components/single-blog/SingleBlog.jsx
+++ src/components/single-blog/SingleBlog.jsx
@@ -15,6 +15,11 @@ import ovalPinkSmall from 'assets/icons/oval-pink-small.svg';
 import { ReactComponent as PreviousButton } from 'assets/icons/previous-button.svg';
 import { ReactComponent as NextButton } from 'assets/icons/next-button.svg';
 import { ReactComponent as EllipsisButton } from 'assets/icons/ellipsis-button.svg';
+import shareIcon from 'assets/icons/share-icon.svg';
+import linkIcon from 'assets/icons/link-icon.svg';
+import fbGreen from 'assets/icons/fb-green.svg';
+import messengerIcon from 'assets/icons/messenger.svg';
+import linkedinIcon from 'assets/icons/linkedin.svg';
 
 import * as S from './single-blog-components';
 
@@ -47,7 +52,108 @@ const SingleBlog = () => {
       <S.HeaderWrapper>
         <Header />
       </S.HeaderWrapper>
-      <S.Body>bodyy </S.Body>
+      <S.Body>
+        <S.ShareWrapper>
+          <S.ShareText>გამოგვყევი:</S.ShareText>
+          <S.IconsWrapper>
+            <S.ShareIcon src={shareIcon} alt="icon" />
+            <S.ShareIcon src={linkIcon} alt="icon" />
+            <S.ShareIcon src={fbGreen} alt="icon" />
+            <S.ShareIcon src={messengerIcon} alt="icon" />
+            <S.ShareIcon src={linkedinIcon} alt="icon" />
+          </S.IconsWrapper>
+        </S.ShareWrapper>
+
+        <S.MainImage
+          src="https://929voice.fm/wp-content/uploads/sites/293/2020/10/young-adult-nathan-van-de-graaf-unsplash-2.jpg"
+          alt="mainImage"
+        />
+        <S.Content>
+          <S.Heading>
+            შეუძლებელია ვერ შეამჩნიოთ რა ხდება საქართველოში ინოვაციებისა და
+            მეწარმეობის
+          </S.Heading>
+          <S.SubHeading>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, აქსელერატორს მეორეინოვაციებისა და
+            მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე მოყვა, ერთ
+            ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე
+          </S.SubHeading>
+
+          <S.SmallImage
+            src="https://929voice.fm/wp-content/uploads/sites/293/2020/10/young-adult-nathan-van-de-graaf-unsplash-2.jpg"
+            alt="smallImage"
+          />
+
+          <S.Text>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე…
+          </S.Text>
+
+          <S.Text>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, აქსელერატორს მეორეინოვაციებისა და
+            მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე მოყვა, ერთ
+            ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე
+          </S.Text>
+
+          <S.Text>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე…
+          </S.Text>
+
+          <S.TextMarked>
+            <span>
+              <i>
+                “ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას
+                მეორე მოყვა, ერთ ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს
+                მეორე”
+              </i>
+            </span>
+          </S.TextMarked>
+
+          <S.Text>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე…
+          </S.Text>
+
+          <S.Text>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, აქსელერატორს მეორეინოვაციებისა და
+            მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე მოყვა, ერთ
+            ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე
+          </S.Text>
+
+          <S.BulletTextWrapper>
+            <S.GreenDot />
+            <S.BulletText>
+              ინოვაციებისა და მეწარმეობის მიმარერთ ორგანიზაციას მეორე მოყვა, ერთ
+              ღონისძიებას
+            </S.BulletText>
+          </S.BulletTextWrapper>
+          <S.BulletTextWrapper>
+            <S.GreenDot />
+            <S.BulletText>
+              ინოვაციებისა და მეწარმეობის მიმარერთ ორგანიზაციას მეორე მოყვა, ერთ
+              ღონისძიებას
+            </S.BulletText>
+          </S.BulletTextWrapper>
+          <S.BulletTextWrapper>
+            <S.GreenDot />
+            <S.BulletText>
+              ინოვაციებისა და მეწარმეობის მიმარერთ ორგანიზაციას მეორე მოყვა, ერთ
+              ღონისძიებას
+            </S.BulletText>
+          </S.BulletTextWrapper>
+
+          <S.Text>
+            ინოვაციებისა და მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე
+            მოყვა, ერთ ღონისძიებას მეორე, აქსელერატორს მეორეინოვაციებისა და
+            მეწარმეობის მიმართულებით, ერთ ორგანიზაციას მეორე მოყვა, ერთ
+            ღონისძიებას მეორე, ერთ ბიზნეს აქსელერატორს მეორე
+          </S.Text>
+        </S.Content>
+      </S.Body>
 
       <S.OtherStories>
         <S.OtherHeading>სხვა ისტორიები</S.OtherHeading>
@@ -75,15 +181,21 @@ const SingleBlog = () => {
         <S.OvalPink src={isMobile ? ovalPinkSmall : ovalPink} />
       </S.OtherStories>
 
-      <S.SubscriptionWrapper>
-        <SubscribeOffer
-          cicrcleColor="#9AB7FF"
-          arrowColor="#9AB7FF"
-          inputColor="pink"
-        />
-      </S.SubscriptionWrapper>
+      <S.ZIndexTop>
+        <S.BreakDiv />
+      </S.ZIndexTop>
 
-      <Footer />
+      <S.ZIndexTop>
+        <S.SubscriptionWrapper>
+          <SubscribeOffer
+            cicrcleColor="#9AB7FF"
+            arrowColor="#9AB7FF"
+            inputColor="pink"
+          />
+        </S.SubscriptionWrapper>
+
+        <Footer />
+      </S.ZIndexTop>
     </S.Wrapper>
   );
 };
--- src/components/single-blog/single-blog-components.jsx
+++ src/components/single-blog/single-blog-components.jsx
@@ -28,7 +28,11 @@ export const SubscriptionWrapper = styled.div`
 `;
 
 export const Body = styled.div`
-  padding-top: 9.8rem;
+  padding: 10.6rem 10% 8.7rem 10%;
+
+  ${({ theme }) => theme.mobile`
+    padding: 10.6rem 0rem 8.7rem 0rem;
+`}
 `;
 
 export const OtherStories = styled.div`
@@ -36,13 +40,18 @@ export const OtherStories = styled.div`
   background-color: #ffffff;
   height: 65.3rem;
   padding: 4rem 10%;
-  margin-bottom: 6.7rem;
   position: relative;
 
   ${({ theme }) => theme.mobile`
      height: unset;
      margin-bottom: 0rem;
   `}
+`;
+
+export const BreakDiv = styled.div`
+  width: 100%;
+  height: 6.7rem;
+  background-color: #f2f1f1;
 `;
 
 export const OtherHeading = styled.p`
@@ -96,3 +105,176 @@ export const ZIndexTop = styled.div`
   z-index: 1;
   position: relative;
 `;
+
+export const MainImage = styled.img`
+  height: 44.8rem;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 4rem;
+
+  ${({ theme }) => theme.mobile`
+   height:27rem;
+  `}
+`;
+
+export const Content = styled.div`
+  max-width: 62.8rem;
+  margin-left: 7rem;
+  margin-top: 3.2rem;
+
+  ${({ theme }) => theme.mobile`
+   margin: 0rem;
+   padding: 0rem 7%;
+`}
+`;
+
+export const Heading = styled.p`
+  font-size: 2.4rem;
+  line-height: 3.4rem;
+  font-weight: 600;
+  color: #13182b;
+  margin-bottom: 2.7rem;
+
+  ${({ theme }) => theme.mobile`
+   font-size: 1.4rem;
+   line-height: 2.2rem;
+   margin-top: 1.6rem;
+   
+`}
+`;
+
+export const SubHeading = styled.p`
+  font-size: 1.8rem;
+  line-height: 2.6rem;
+  font-weight: 500;
+  color: #13182b;
+  margin-bottom: 3.2rem;
+
+  ${({ theme }) => theme.mobile`
+   font-size: 1.4rem;
+   line-height: 2.2rem;
+   margin-top: 1.6rem;
+`}
+`;
+
+export const SmallImage = styled.img`
+  height: 23.2rem;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 4rem;
+  margin-bottom: 3.2rem;
+`;
+
+export const Text = styled.p`
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+  font-weight: 400;
+  color: #13182b;
+  margin-bottom: 2.7rem;
+
+  ${({ theme }) => theme.mobile`
+   font-size: 1.4rem;
+   line-height: 2.2rem;
+   margin-top: 1.6rem;
+`}
+`;
+
+export const TextMarked = styled.p`
+  font-size: 3.2rem;
+  line-height: 4.5rem;
+  font-weight: 500;
+  color: #13182b;
+  margin-bottom: 3.2rem;
+  text-align: center;
+
+  & span {
+    -webkit-box-decoration-break: clone;
+    box-decoration-break: clone;
+    background-color: #ffca0f;
+    padding: 0rem 1rem;
+  }
+
+  ${({ theme }) => theme.mobile`
+   font-size: 1.8rem;
+   line-height: 3.1rem;
+   margin: 2rem 0rem;
+`}
+`;
+
+export const BulletTextWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin: 2.7rem 0rem;
+`;
+
+export const GreenDot = styled.div`
+  width: 1.6rem;
+  height: 1.6rem;
+  background-color: #55aa79;
+  border-radius: 50%;
+  margin-right: 2.5rem;
+  margin-top: 0.4rem;
+`;
+
+export const BulletText = styled.p`
+  font-size: 1.8rem;
+  line-height: 2.5rem;
+  font-weight: 400;
+  color: #13182b;
+  max-width: 52rem;
+
+  ${({ theme }) => theme.mobile`
+   font-size: 1.4rem;
+   line-height: 2rem;
+   margin: 2rem 0rem;
+   max-width: 33rem;
+   margin: unset;
+`}
+`;
+
+export const ShareWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  color: #767c8b;
+  font-size: 1.6rem;
+  font-weight: 400;
+  right: 12%;
+  top: 58rem;
+
+  ${({ theme }) => theme.mobile`
+    position: absolute;
+    top: 10rem;
+    right: 4rem;
+    z-index: 9;
+  `}
+`;
+
+export const ShareIcon = styled.img`
+  margin-right: 1.4rem;
+  width: 4.2rem;
+  height: 4.2rem;
+  cursor: pointer;
+
+  ${({ theme }) => theme.mobile`
+    margin-right: unset;
+    margin-bottom: 1rem;
+  `}
+`;
+
+export const ShareText = styled.p`
+  ${({ theme }) => theme.mobile`
+   display: none;
+`}
+`;
+
+export const IconsWrapper = styled.div`
+  display: flex;
+  margin-top: 1.1rem;
+
+  ${({ theme }) => theme.mobile`
+    flex-direction: column;
+  `}
+`;
+
+export const IconsMobileWrapper = styled.div``;
